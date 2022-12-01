03c45dd1a198d2c1c1cf87052269b35d3a0c706f
Costin-Robert Sin <sin.costinrobert@gmail.com>
2022-12-01 18:21:48 +0200

    Add day template
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

---
+++ src/solutions/template.rs
@@ -0,0 +1,15 @@
+use std::error::Error;
+
+#[allow(dead_code)]
+pub fn part1(input: &String) -> Result<(), Box<dyn Error>> {
+    println!("Not implemented");
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn part2(input: &String) -> Result<(), Box<dyn Error>> {
+    println!("Not implemented");
+
+    Ok(())
+}
