39618856bfbbd62c48694e5379e36e84b96af09b
Costin-Robert Sin <sin.costinrobert@gmail.com>
2022-12-04 12:09:38 +0200

    Rewrite day 3 for better understanding
    Signed-off-by: Costin-Robert Sin <sin.costinrobert@gmail.com>

--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -40,16 +40,18 @@ pub fn part1(input: &String) -> Result<(), Box<dyn Error>> {
 pub fn part2(input: &String) -> Result<(), Box<dyn Error>> {
     let lines = input.split("\n").collect::<Vec<&str>>();
 
-    let score = (0..lines.len() / 3)
-        .map(|i| {
-            let set1: HashSet<char> = HashSet::from_iter(lines[3 * i].chars());
-            let set2: HashSet<char> = HashSet::from_iter(lines[3 * i + 1].chars());
-            let set3: HashSet<char> = HashSet::from_iter(lines[3 * i + 2].chars());
+    let score = lines
+        .chunks(3)
+        .map(|group_of_3| {
+            let [l1, l2, l3] = group_of_3 else {
+                unreachable!("Total number of lines should be divisible by 3")
+            };
 
-            let i12 = set1
-                .intersection(&set2)
-                .map(|&e| e)
-                .collect::<HashSet<char>>();
+            let set1: HashSet<char> = HashSet::from_iter(l1.chars());
+            let set2: HashSet<char> = HashSet::from_iter(l2.chars());
+            let set3: HashSet<char> = HashSet::from_iter(l3.chars());
+
+            let i12 = set1.intersection(&set2).cloned().collect::<HashSet<char>>();
             let Some(&badge) = i12.intersection(&set3).next() else {
                 unreachable!("There should be exactly one badge")
             };
